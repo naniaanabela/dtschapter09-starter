95ef99f1ed99d1bf047057cd1cf86fa4ec00dffb
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2019-11-25 15:17:49 +0700

    Menjalankan Operasi SELECT

--- DTSApp/app/src/main/java/polinema/ac/id/dtsapp/ProfileActivity.java
+++ DTSApp/app/src/main/java/polinema/ac/id/dtsapp/ProfileActivity.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import polinema.ac.id.dtsapp.data.AppDbProvider;
+import polinema.ac.id.dtsapp.data.User;
+import polinema.ac.id.dtsapp.data.UserDao;
+
 
 public class ProfileActivity extends AppCompatActivity
 {
@@ -17,6 +21,8 @@ public class ProfileActivity extends AppCompatActivity
     private EditText edtEmail;
     private EditText edtPhoneNumber;
     private Button btnSave;
+
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,11 @@ public class ProfileActivity extends AppCompatActivity
 
     private void loadData()
     {
+        // Mendatapatkan DAO dari DTSAppDatabase
+        UserDao daoUser = AppDbProvider.getInstance(this).userDao();
 
+        // Melakukan SELECT terhadap 1 user yang paling awal, dan mengembalikan hasilnya ke property currentUser
+        this.currentUser = daoUser.selectOne();
     }
 
     private void initComponents()
@@ -39,6 +49,19 @@ public class ProfileActivity extends AppCompatActivity
         this.edtEmail = this.findViewById(R.id.edt_email);
         this.edtPhoneNumber = this.findViewById(R.id.edt_phone_number);
         this.btnSave = this.findViewById(R.id.btn_save);
+
+        // Jika tidak ada data registrasi sebelumnya, tidak perlu melakukan apa-apa, dan matikan Button Save agar user tidak menyimpan data kosong.
+        if(this.currentUser == null)
+        {
+            this.btnSave.setEnabled(false);
+            return;
+        }
+
+        // Menyalin data dari property currentUser ke semua komponen yang sesuai
+        this.edtUsername.setText(this.currentUser.username);
+        this.edtPassword.setText(this.currentUser.password);
+        this.edtEmail.setText(this.currentUser.email);
+        this.edtPhoneNumber.setText(this.currentUser.phoneNumber);
     }
 
     public void onBtnSave_Click(View view)
